e1b8dd2a39d537aea7a7d8a49d2633ed518bdfd5
karna <karnagogoi7@gmail.com>
2022-07-12 13:43:39 +0530

    Add managers stats to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import data from "../data";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    managerStats: data,
+  },
   mutations: {},
   actions: {},
   modules: {},
