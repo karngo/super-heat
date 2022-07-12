913bb4e0d76df831929bdbf66390f6be07207e4f
karna <karnagogoi7@gmail.com>
2022-07-12 12:32:56 +0530

    Add heatmap view

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <v-app>
     <v-main>
       <h2>Super Heat</h2>
+      <HeatMap />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import HeatMap from "./views/HeatMap.vue";
+
 export default {
   name: "App",
+  components: {
+    HeatMap,
+  },
   data: () => ({
     //
   }),
---
+++ src/views/HeatMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card outlined>
+          <v-card-text>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class="pa-2">Name 1</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  name: "HeatMap",
+};
+</script>
