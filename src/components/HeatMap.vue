0a649c07af333621f03c151f74302609179c86f7
karna <karnagogoi7@gmail.com>
2022-07-13 19:21:11 +0530

    Refactoring
    Add heatmap component

---
+++ src/components/HeatMap.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-simple-table class="heatmap">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-center">
+            <v-icon dense>{{ yAxis.icon }}</v-icon>
+            {{ yAxis.title }}
+          </th>
+          <th
+            v-for="(xValue, yIndex) in xValues"
+            :key="yIndex"
+            class="text-center"
+          >
+            {{ xValue }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(yValue, xIndex) in yValues" :key="xIndex">
+          <td>
+            <v-card outlined class="text-center grey darken-3">
+              <v-card-text class="py-1 white--text">
+                {{ yValue }}
+              </v-card-text>
+            </v-card>
+          </td>
+          <td
+            v-for="(tableValue, tIndex) in tableData[xIndex]"
+            :key="tIndex"
+            class="px-1"
+          >
+            <v-card
+              outlined
+              class="text-center"
+              :color="getHeatColor(tableValue)"
+            >
+              <v-card-text class="pa-1">
+                {{ tableValue }}
+              </v-card-text>
+            </v-card>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+<script>
+export default {
+  name: "HeatMap",
+  props: {
+    yAxis: Object,
+    yValues: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    xValues: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    tableData: Array,
+  },
+  methods: {
+    getHeatColor(value) {
+      if (value * 1 < 2) return "error";
+      if (value * 1 < 4) return "warning";
+      return "success";
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.heatmap ::v-deep {
+  tbody tr td:first-child {
+    width: 20%;
+  }
+}
+</style>
